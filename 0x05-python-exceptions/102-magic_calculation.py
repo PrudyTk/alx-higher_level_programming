2036c7389cac9364d96446aa1693ff3cd91e71cb
PrudyTk <prudyytk@gmail.com>
2023-06-27 05:20:38 -0700

    0x05. Python - Exceptions

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             else:
                 result += a ** b / i
-        except ValueError as i:
+        except ValueError as e:
             result = b + a
             break
     return (result)
